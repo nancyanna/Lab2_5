27940291dc431692b312b39bb8bf4ffb48c6c50f
nancyanna <61117785+nancyanna@users.noreply.github.com>
2020-02-18 14:16:19 -0600

    Add files via upload

---
+++ ConsoleApp2_5_4/ConsoleApp2_5_4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp2_5_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            Console.WriteLine("Enter your age");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You look younger than" + " " + age);
+
+        }
+    }
+}
